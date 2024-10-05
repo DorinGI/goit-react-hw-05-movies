5b48883e8c453adeab3b27ffe82f954b4062f164
DorinGI <158717197+DorinGI@users.noreply.github.com>
2024-10-05 16:53:01 +0200

    initial commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+const API_KEY = '686954a6867702e2802dd31dcf4680f7';
+
 export const App = () => {
   return (
     <div
@@ -7,7 +9,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
